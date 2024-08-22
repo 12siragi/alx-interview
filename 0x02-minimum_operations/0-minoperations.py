5cde60c8a8024806bde177a84d4a6b8f62578940
12siragi <siragisaidi218@gmail.com>
2024-08-22 19:11:35 +0000

    algorithm

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Module that calculates the minimum number of operations needed
-to get exactly n H characters in the file.
+Module that calculates the minimum number of .
 """
 
+
 def minOperations(n):
-    """Returns the fewest number of operations needed to result in exactly n H characters."""
+    """Returlt in exactly n H characters."""
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
 
@@ -17,5 +17,5 @@ def minOperations(n):
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
