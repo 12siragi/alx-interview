06f9314ec3d527bb00524ce058f0de869050920a
12siragi <siragisaidi218@gmail.com>
2024-10-19 20:26:10 +0000

    UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,46 +1,40 @@
 #!/usr/bin/python3
-"""
-A method that determines if a given data set represents
-a valid UTF-8 encoding.
-"""
+""" UTF-8 Validation """
 
 
 def validUTF8(data):
     """
-    Function to validate UTF-8 encoding.
-    
-    Args:
-        data (list): Data set containing multiple characters, where each integer
-                     represents 1 byte of data.
-    
-    Returns:
-        bool: True if the data is valid UTF-8 encoded, otherwise False.
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
     """
-    utf8valid = 0
-    
-    for val in data:
-        # Consider only the least significant 8 bits (1 byte)
-        byte = val & 255
-        
-        # If we're expecting continuation bytes
-        if utf8valid:
-            # Check if the byte is a valid continuation byte (10xxxxxx)
-            if byte >> 6 != 0b10:
-                return False
-            utf8valid -= 1
-            continue
-        
-        # Determine the number of bytes in the current UTF-8 character
-        while (1 << (7 - utf8valid)) & byte:
-            utf8valid += 1
-        
-        # If utf8valid is 1 (invalid scenario) or greater than 4, it's invalid
-        if utf8valid == 1 or utf8valid > 4:
-            return False
-        
-        # If the byte represents a single-byte character, reset utf8valid
-        utf8valid = max(utf8valid - 1, 0)
-    
-    # After processing all data, ensure there are no pending continuation bytes
-    return utf8valid == 0
+    number_bytes = 0
 
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
